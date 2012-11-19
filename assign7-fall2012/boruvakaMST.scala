296f8670f30227311e109b8be34585963967527b
John Salvatier <jsalvatier@gmail.com>
2012-11-19 14:08:13 -0800

    figured out the main difficulty

--- assign7-fall2012/boruvakaMST.scala
+++ assign7-fall2012/boruvakaMST.scala
@@ -11,10 +11,52 @@ object boruvaka {
     def MST(es : Seq[Edge], n : Int) : Seq[Edge];
   }
 
+  object MST_Seq extends MST_Solver {
+    /* cost bounds : 
+    the cost bounds specified are 
+      W <= m log n + n 
+      S <= (log n)**k for some k 
+
+    but the sorting of the edge list they suggest plainly violates the cost bounds unless you use some kind of non-comparison sort (which
+      has not been covered)
+
+    thus we assume that it's going to be the same cost bounds EXCEPT for the initial sort 
+     */
+    type EdgeL = (Vertex, Vertex, Weight, Int)
+    def MST(es : Seq[Edge], n : Int) : Seq[Edge] = { 
+      val ses = es.sorted( by[Edge, Weight](_.3))
+      val nes = es map {case (u, v, w) => (u, v, w, i)}
+
+      val esl = MST_(  , nes,List(), 0)
+      esl flatmap (l => l map ( es(_) )
+    }
+    type VVMap = Array[Vertex]
+    def MST_(vs : Array[Vertex], es : Seq[EdgeL], t : List[List[Int]], i : Int) : List[List[Int]] =  
+      if (e.size == 0) 
+        t
+      else { 
+        val (c, p) = minStarContract(vs, es, i)
+        val remapped = map {case (u, v,w,l) => (p(u), p(v), w, l)} 
+        val nne = remapped filter {case (u, v,w, l) => u != v} 
+        MST_(n, nne, c ::t , i + 1)
+      }
+
+    def minStartContract( vs : Array[Vertex], es : Seq[EdgeL], i : Int)  : (Seq[EdgeL], VVMap)= {
+      val heads = Vector.tabulate(vs.size)( n => i * 71 % 2 == 0) 
+      
+      val mins = minEdges(vs, es)
+      val contract = mins filter {case (u, v, w, l) => !heads(u) && heads(v)}
+
+      val rm = Array.tabulate(vs.size)
+      val remap = rm update (contract map {case (u, v, w, l) => u -> v})
+      (contract, remap) 
+    }
+    def minEdges(vs : Array[Vertex], es : Seq[EdgeL]) : Seq[EdgeL] = {
+
+    }
+  }
   object MST_Map extends MST_Solver {
-    type Graph[L] = Map[Vertex, Map[Vertex, Double,L]] 
-    type G = Graph[Edg]
-    type E = Edge[Edg] 
+    type Graph = Seq[Seq[Vertex]] 
 
     def makeGraph[L]( es :  Seq[Edge] ) : Graph[L] = 
       (es map single) reduce merge
@@ -37,7 +79,7 @@ object boruvaka {
 
     }
     def starContract(g : Graph[L], es : Set[Edge[L]], r : Random) : (Graph[L], Set[Edge[L]]) = { 
-      val heads = Vector.tabulate(g.size)(r.nextBoolean)
+      val heads = Vector.tabulate(g.size)(r.nextBoolean) // assume O(n) work  O(log n) span
       val ces = es.filter {case ((a,b, w), _) => !heads(a) && heads(b)}
       val v = Map(ces map {case ((a,b,w), _) => a -> b})
       (merge(g)(v), ces)
